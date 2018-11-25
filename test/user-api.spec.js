84030fb858cd9c850814a8d3fbcbe5568d2d638b
Deepak Acharya Santhekatte <aacharyadeepak100@gmail.com>
2018-11-25 15:43:08 +0530

    updated data

--- test/user-api.spec.js
+++ test/user-api.spec.js
@@ -1,6 +1,10 @@
+require('co-mocha');
+var should=require('should');
+var data =require('../user-data');
 describe('user data', function() {
     it('should have +1 user count after saving', function*() {
         var users = yield data.users.get();
+        console.log(users);
         yield data.users.save({
             name: 'Deepak'
         });
---
+++ user-data.js
@@ -0,0 +1,13 @@
+var fs=require('fs');
+var userFile='./users.json';
+module.exports = {
+    users: {
+        get: function*() {
+            var data = yield fs.readFile(userFile, 'utf-8');
+            return JSON.parse(data);
+        },
+        save: function*() {
+
+        } 
+    }
+}
