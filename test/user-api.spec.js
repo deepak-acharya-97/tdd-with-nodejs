515b0208527dcbf22b83f0e118b77585e80c3dc6
Deepak Acharya Santhekatte <aacharyadeepak100@gmail.com>
2018-11-25 12:21:21 +0530

    updated one simple test case

---
+++ test/user-api.spec.js
@@ -0,0 +1,10 @@
+describe('user data', function() {
+    it('should have +1 user count after saving', function*() {
+        var users = yield data.users.get();
+        yield data.users.save({
+            name: 'Deepak'
+        });
+        var newUsers = yield data.users.get();
+        newUsers.length.should.equal(users.length + 1);
+    })
+})
